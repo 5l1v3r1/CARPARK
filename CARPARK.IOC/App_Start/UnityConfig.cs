200c6a503599569dece4fabf9d459cf92012a922
AhmetTuranAlp <turan.alp@ogr.sakarya.edu.tr>
2018-04-23 13:28:57 +0300

    IOC UnitConfig
    IOC Katmanında UnitConfig yapısı oluşturuldu.

---
+++ CARPARK.IOC/App_Start/UnityConfig.cs
@@ -0,0 +1,30 @@
+using CARPARK.Data.UnitofWork;
+using System;
+using System.Web.Mvc;
+using Unity;
+using Unity.Lifetime;
+using Unity.Mvc5;
+
+namespace CARPARK.IOC
+{
+    //Unity.MVC5 Dosyasý Nuget'den kurulumu yapýldý.
+    public static class UnityConfig
+    {
+        public static void RegisterComponents()
+        {
+			var container = new UnityContainer();
+            RegisterTypes(container);
+            DependencyResolver.SetResolver(new UnityDependencyResolver(container));
+        }
+
+        private static void RegisterTypes(UnityContainer container)
+        {
+            container.BindInRequestScope<IUnitofWork, UnitofWork>();
+
+        }
+        public static void BindInRequestScope<T1, T2>(this IUnityContainer container) where T2 : T1
+        {
+            container.RegisterType<T1, T2>(new HierarchicalLifetimeManager());
+        }
+    }
+}
