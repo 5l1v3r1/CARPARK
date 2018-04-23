e8fba985ea9c3d4bcb199778ca98f5bac4216133
AhmetTuranAlp <turan.alp@ogr.sakarya.edu.tr>
2018-04-23 13:35:41 +0300

    DTO Sınıf Oluşturuldu.
    Personel, KullanıcıYetki, Uye sınıflarının DTO sınıf katmanları oluşturuldu.

---
+++ CARPARK.DTO/EntitisDTO/KullaniciYetkiDTO.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CARPARK.DTO.EntitisDTO
+{
+   public class KullaniciYetkiDTO
+    {
+        public int YetkiID { get; set; }
+        public string YetkiAdi { get; set; }
+    }
+}
---
+++ CARPARK.DTO/EntitisDTO/UyeDTO.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CARPARK.DTO.EntitisDTO
+{
+    public class UyeDTO
+    {
+        public int UyeID { get; set; }
+        public string KullaniciAdi { get; set; }
+        public string Parola { get; set; }
+        public string Eposta { get; set; }
+    }
+}
